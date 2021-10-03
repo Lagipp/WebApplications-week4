0abab04c9a25fb95727b0de4b668622c92471478
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-10-03 15:30:21 +0300

    maybe now?

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/recipe/:food', function(req, res, next) {
   //res.send(req.params.food);
   JSONrecipe = {name:req.params.food, instructions:"", ingredients:""}
   //res.json({name:req.params.food, instructions:"", ingredients:""})
-  res.send(JSONrecipe.name);
+  res.send(JSONrecipe);
   console.log("The recipe you wanted was: " + req.params.food);
   console.log("in JSON: " + JSONrecipe.name);
 });
