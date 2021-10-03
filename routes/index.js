b1d04a45ee17d6c65ca5c08dc1e573d45c2af4e7
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-10-03 15:24:03 +0300

    part 1 done maybe now?

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -11,8 +12,12 @@ router.get('/', function(req, res, next) {
 
 /* GET food page. */
 router.get('/recipe/:food', function(req, res, next) {
-  res.send(req.params.food);
-  //console.log(req.params.food);
+  //res.send(req.params.food);
+  JSONrecipe = {name:req.params.food, instructions:"", ingredients:""}
+  //res.json({name:req.params.food, instructions:"", ingredients:""})
+  res.send(JSONrecipe.name);
+  console.log("The recipe you wanted was: " + req.params.food);
+  console.log("in JSON: " + JSONrecipe.name);
 });
 
 module.exports = router;
