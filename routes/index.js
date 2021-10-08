a402a4a374ea1a26599eece1ef827442c5cb6a91
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-10-08 12:21:54 +0300

    99.892189 % done

--- routes/index.js
+++ routes/index.js
@@ -31,8 +31,14 @@ router.get('/recipe/:food', function(req, res, next) {
 
 /* GET recipe page */
 router.get('/recipe', function(req, res, next) {
+  // console.log("--DEBUG: typeof 'req': " + typeof req);
+  // console.log("--DEBUG: typeof 'req.body': " + typeof req.body);
+  // console.log("--DEBUG: typeof 'dataList': " + typeof dataList);
+  // console.log("*** DEBUG: printing 'dataList': " + (dataList));
+  //console.log(JSON.parse(req.body));
+  //res.json(req.body);
+  console.log("** inside 'req.body' of GET /recipe: " + JSON.stringify(req.body));
   res.send(req.body);
-  //res.send("HELLO WORLD!");
 })
 
 
@@ -40,7 +46,7 @@ router.get('/recipe', function(req, res, next) {
 /* create POST route for recipe. */
 router.post('/recipe', function(req, res, next) {
   console.log("== DEBUG: POST route in 'index.js': " + JSON.stringify(req.body));
-  res.send(JSON.stringify(req.body));
+  res.send(req.body);
 
   /* https://masteringjs.io/tutorials/express/body */
 
